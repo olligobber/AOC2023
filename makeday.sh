7bb544b98b91fa3200d3072a4053fbce3d9b7e80
olligobber <sirolligobbervii@yahoo.com.au>
2023-12-02 17:12:37 +1100

    Neatened directory structure, improved setup script

--- makeday.sh
+++ makeday.sh
@@ -3,31 +3,41 @@
 # Go to "developer tools -> application -> cookies" in a logged in browser to get it
 
 # This won't work well if you run this before midnight and AOC starts at or after midnight in your timezone
-today=$(date -d "today" +%d)
-trimday=$(date -d "today" +%-d)
+today=$(date +%d)
+trimday=$(date +%-d)
 
 # This won't work well if your local time zone isn't a whole number of hours plus or minus UTC
 localunlocktime=$(date -d 'TZ="EST" 12am' +%-l%P)
 
-# Check if the directory already exists
-if [ -d "Day$today" ]; then
-	echo "Directory for day $today already exists!"
-	exit 1
-fi
-
 echo "Setting up day $today"
 
-# Make the directory
-mkdir "Day$today"
-
-# Copy over the template files
-sed "s/Day/Day$today/g" < "Template/Template.cabal" > "Day$today/Day$today.cabal"
-mkdir "Day$today/app"
-cp "Template/Template.hs" "Day$today/app/Part1.hs"
-cp "Template/Template.hs" "Day$today/app/Part2.hs"
+# Check if the directory already exists
+if [ -d "Day$today" ]; then
+	echo "  Directory for day $today already exists!"
+else
+	# Make the directory
+	mkdir "Day$today"
+fi
 
 # Go into the directory
 cd "Day$today"
+
+# Copy over the template files
+if [ -f "Day$today.cabal" ]; then
+	echo "  Cabal file already exists!"
+else
+	sed "s/Day/Day$today/g" < "../Template/Template.cabal" > "Day$today.cabal"
+fi
+if [ -f "Part1.hs" ]; then
+	echo "  Source for part 1 already exists!"
+else
+	cp "../Template/Template.hs" "Day$today/Part1.hs"
+fi
+if [ -f "Part2.hs" ]; then
+	echo "  Source for part 2 already exists!"
+else
+	cp "Template/Template.hs" "Day$today/Part2.hs"
+fi
 
 echo "Starting cabal build, this may take a moment"
 
@@ -35,6 +45,12 @@ echo "Starting cabal build, this may take a moment"
 cabal build -v0
 
 echo "Cabal build complete"
+
+# Check if input exists
+if [ -f "input" ]; then
+	echo "  Input data already exists!"
+	exit 0
+fi
 
 # Check if session cookie is set up
 if [ ! -f "../session_cookie" ]; then
