6ae9e91abf206814a9844f6ea215dc1653579bcb
olligobber <sirolligobbervii@yahoo.com.au>
2023-12-09 19:37:39 +1300

    Make adding previous days easier

--- makeday.sh
+++ makeday.sh
@@ -1,15 +1,29 @@
 # Set up the folder and inputs for a day of Advent Of Code
+
+# The first argument if provided is the day to set up, and defaults to the current day in local time
+# The second argument if provided is the year to set up, and defaults to the current year in local time
+
 # The session cookie for adventofcode.com should be located in ./session_cookie
 # Go to "developer tools -> application -> cookies" in a logged in browser to get it
 
-# This won't work well if you run this before midnight and AOC starts at or after midnight in your timezone
-today=$(date +%d)
-trimday=$(date +%-d)
+if [ $# -gt 0 ]; then 
+	today=$(date -d "December $1" +%d)
+	trimday=$(date -d "December $1" +%-d)
+else 
+	today=$(date +%d)
+	trimday=$(date +%-d)
+fi
+
+if [ $# -gt 1 ]; then 
+	year=$2
+else 
+	year=$(date +%Y)
+fi 
 
 # This won't work well if your local time zone isn't a whole number of hours plus or minus UTC
 localunlocktime=$(date -d 'TZ="EST" 12am' +%-l%P)
 
-echo "Setting up day $today"
+echo "Setting up day $today for year $year"
 
 # Check if the directory already exists
 if [ -d "Day$today" ]; then
@@ -31,12 +45,12 @@ fi
 if [ -f "Part1.hs" ]; then
 	echo "  Source for part 1 already exists!"
 else
-	cp "../Template/Template.hs" "Day$today/Part1.hs"
+	cp "../Template/Template.hs" "Part1.hs"
 fi
 if [ -f "Part2.hs" ]; then
 	echo "  Source for part 2 already exists!"
 else
-	cp "../Template/Template.hs" "Day$today/Part2.hs"
+	cp "../Template/Template.hs" "Part2.hs"
 fi
 
 echo "Starting cabal build, this may take a moment"
@@ -60,13 +74,13 @@ fi
 
 # Download the input file
 cookie=$(cat ../session_cookie)
-timeuntil=$(($(date -d "TZ=\"EST\" December $today 12am" +%s) - $(date +%s)))
+timeuntil=$(($(date -d "TZ=\"EST\" December $today $year 12am" +%s) - $(date +%s)))
 if [ $timeuntil -gt 0 ]; then
 	echo "Waiting until $localunlocktime to download input"
 	sleep "$timeuntil"s
 fi
 echo "Downloading input now"
-wget -q --header "Cookie: session=$cookie" "https://adventofcode.com/2023/day/$trimday/input"
+wget -q --header "Cookie: session=$cookie" "https://adventofcode.com/$year/day/$trimday/input"
 
 # Report success/failure
 if [ -f "input" ]; then
